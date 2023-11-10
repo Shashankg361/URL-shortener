5a82310508d1878b3850af9736633ea3913266b5
shashankg361 <shashankg361@gmail.com>
2023-11-10 23:11:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [Url , setUrl] = useState('');
+  const [shortUri , setShortUri] = useState('')
+  const formRef = useRef(null);
+   
+  const handelSubmit =async(event)=>{
+    event.preventDefault();
+    console.log('entered')
+    const response = await axios(`http://localhost:8000/?q=${Url}`)
+    const data = response.data;
+    console.log('got data from backend - ', data)
+    setShortUri(data.Message) ; 
+  }
+
+  shortUri && console.log("Link - ",shortUri);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className='font-medium text-2xl m-5' >URL shortener</h1>
       </header>
+      <main>
+        <form ref={formRef} onSubmit={handelSubmit}>
+          <input onChange={(e)=>setUrl(e.target.value)} className='border border-black p-1 mr-2 rounded-lg' id='URL' type='text' placeholder='Enter URl'></input>
+          <input type='submit' id='URL' className='border-2 p-1 border-black cursor-pointer rounded-lg'></input>
+        </form>
+        <div className='m-3 p-2 bg-black text-white'>
+          <h2>Here is your shorten URI</h2>
+          <h1>Link - <a target='blank' href={shortUri}>{shortUri}</a></h1>
+        </div>
+      </main>
     </div>
   );
 }
